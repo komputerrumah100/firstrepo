929f89853ee546401d5aff3f4aed6c249e29f055
komputerrumah100 <komprumah100@gmail.com>
2020-06-29 17:21:55 +0700

    test

---
+++ cssVariable/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+// Ini Tulisan dengan box keren
+const block = document.querySelector('.block');
+document.documentElement.style.setProperty('--corner','35px');
+
+
+// Ini Gambar dengan variable css
+const article = document.querySelector('.article');
+document.documentElement.style.setProperty('--img','url("bgr1.jpg")');
+
