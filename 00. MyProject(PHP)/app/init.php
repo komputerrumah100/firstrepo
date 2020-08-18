4211edf271d81591f67b4a416122e9537e68f5eb
komputerrumah100 <komprumah100@gmail.com>
2020-08-18 12:49:01 +0700

    commit

--- 00. MyProject(PHP)/app/init.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-require_once '../app/lib/App.php';
-require_once '../app/lib/Controller.php';
-require_once '../app/config/config.php';
-require_once '../app/lib/Database.php';
-
-
--- 00. MyProject(PHP)/app/views/Websites/Home/index.php
+++
@@ -1,3 +0,0 @@
-
-    <h1>Sahabat Xilva</h1>
-
